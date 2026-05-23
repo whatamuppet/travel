de8075582f54fe1bfd9f757634ba2c83d8f12357
Justin <justinrcasteel@gmail.com>
2026-05-23 14:25:53 -0400

    removed new york

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,6 @@ import { Routes } from '@angular/router';
 
 import { Home } from '../pages/home/home';
 import { TurksAndCaicos } from '../pages/turks-and-caicos/turks-and-caicos';
-import { NewYork } from '../pages/new-york/new-york';
 import { Italy } from '../pages/italy/italy';
 import { Switzerland } from '../pages/switzerland/switzerland';
 import { Washingtondc } from '../pages/washingtondc/washingtondc';
@@ -10,7 +9,6 @@ import { Washingtondc } from '../pages/washingtondc/washingtondc';
 export const routes: Routes = [
     { path: '', component: Home },
     { path: 'turks-and-caicos', component: TurksAndCaicos },
-    { path: 'new-york', component: NewYork },
     { path: 'washingtondc', component: Washingtondc },
     { path: 'italy', component: Italy },
     { path: 'switzerland', component: Switzerland },
